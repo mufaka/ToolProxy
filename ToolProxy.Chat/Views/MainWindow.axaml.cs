93705072b15c49b320c0706cf363c2afafacaab1
mufaka <bill.nickel@gmail.com>
2025-08-24 21:05:07 -0700

    Scroll with messages automatically in the chat window.

--- ToolProxy.Chat/Views/MainWindow.axaml.cs
+++ ToolProxy.Chat/Views/MainWindow.axaml.cs
@@ -1,5 +1,7 @@
 using Avalonia.Controls;
 using Avalonia.Input;
+using System.Collections.Specialized;
+using ToolProxy.Chat.ViewModels;
 
 namespace ToolProxy.Chat.Views;
 
@@ -10,17 +12,48 @@ public partial class MainWindow : Window
         InitializeComponent();
 
         MessageInput.AddHandler(KeyDownEvent, OnMessageInputPreviewKeyDown, Avalonia.Interactivity.RoutingStrategies.Tunnel);
+
+        // Subscribe to DataContext changes to hook up auto-scroll
+        DataContextChanged += OnDataContextChanged;
+    }
+
+    private void OnDataContextChanged(object? sender, EventArgs e)
+    {
+        if (DataContext is MainWindowViewModel viewModel)
+        {
+            // Subscribe to collection changes to auto-scroll
+            viewModel.Messages.CollectionChanged += OnMessagesCollectionChanged;
+        }
+    }
+
+    private void OnMessagesCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (e.Action == NotifyCollectionChangedAction.Add)
+        {
+            // Scroll to bottom when new messages are added
+            ChatScrollViewer.ScrollToEnd();
+        }
     }
 
     private void OnMessageInputPreviewKeyDown(object? sender, KeyEventArgs e)
     {
         if (e.Key == Key.Enter && !e.KeyModifiers.HasFlag(KeyModifiers.Shift))
         {
-            if (DataContext is ViewModels.MainWindowViewModel vm)
+            if (DataContext is MainWindowViewModel vm)
             {
                 vm.SendMessageCommand.Execute();
             }
             e.Handled = true;
         }
+    }
+
+    protected override void OnClosed(EventArgs e)
+    {
+        // Clean up event subscriptions
+        if (DataContext is MainWindowViewModel viewModel)
+        {
+            viewModel.Messages.CollectionChanged -= OnMessagesCollectionChanged;
+        }
+        base.OnClosed(e);
     }
 }
